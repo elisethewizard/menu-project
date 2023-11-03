9ece55e5e4f13d95a6cd708fc4f6a009bb3f65b3
Jetixxxx <86999190+Jetixxxx@users.noreply.github.com>
2023-11-03 19:41:01 +0300

    version-1

--- index.js
+++ index.js
@@ -1,30 +1,102 @@
 import { menuArray } from '/data.js'
+const menuContainer = document.getElementById('menu-container')
+const checkoutContainer = document.getElementById('checkout-container')
+const paymentForm = document.getElementById('payment-form')
+let allItemsAddedArr = []
+let totalPrice
 
-function getMenuHtml(){
+function renderMenu(){
     let menuHtml = ``
-    menuArray.forEach(function(item) {
-    menuHtml += `
+    menuArray.forEach(function(menuItem) {
+        menuHtml += `
 <div class='menu-item'>
-    <div class='item-emoji'>${item.emoji}</div>
+    <div class='item-emoji'>${menuItem.emoji}</div>
     <div class='item-desc'>
-        <p class='item-name'>${item.name}</p>
-        <p>${item.ingredients}</p>
-        <p class='item-price'>$${item.price}</p>
+        <p class='item-name no-margin'>${menuItem.name}</p>
+        <p class='item-ingr no-margin'>${menuItem.ingredients}</p>
+        <h3 class='price no-margin'>$${menuItem.price}</h3>
     </div>
-    <button id='add-btn' data-add='${item.id}'>+</button>
+    <button id='${menuItem.id}' data-add='${menuItem.name}'> + </button>
 </div>
 `})
-    return menuHtml
+    menuContainer.innerHTML = menuHtml
 }
 
-function handleAddClick(item){
-    console.log('clicked')
+document.addEventListener('click', function(e){
+    if (e.target.dataset.add) {
+        addToOrder(e.target.dataset.add)
+    } else if (e.target.dataset.remove) {
+        removeFromOrder(e.target.dataset.remove)
+    }
+})
+
+function addToOrder(addedItemName){
+    const addedItemObj = menuArray.filter(function(obj){
+        return obj.name === addedItemName 
+    })[0]
+    allItemsAddedArr.push(addedItemObj)
+    renderOrder()
 }
 
-// document.getElementById('add-btn').addEventListener('click', handleAddClick(e.target.dataset.add))
-
-function render(){
-    document.getElementById('menu-container').innerHTML = getMenuHtml()
+ function renderOrder(){
+    const total = allItemsAddedArr.reduce((total, current) => total + current.price)
+     
+    const addedItemsHtml = allItemsAddedArr.map(element => {
+         return `
+            <div class="checkout-item">
+            <div class='iteml'>
+                <p class='item-name no-margin'>${element.name}</p>
+                <button id='remove-${element.id}' data-remove='${element.name}'>remove</button>
+            </div>
+            <h3 class='price no-margin'>$${element.price}</h3>
+         </div>`}).join('')
+     
+    const orderHtml = `
+         <p id='title'>Your order</p>
+         <div id='checkout-items'>${addedItemsHtml}</div>
+         <div id='total'>
+             <p class='no-margin'>Total price:</p>
+             <h3 class='price no-margin'>$${calculateTotal()}</h3>
+         </div>
+        <button id='complete-order-btn' class='btn-lime'>Complete order</button>`
+    checkoutContainer.innerHTML = orderHtml
+    document.getElementById('complete-order-btn').addEventListener('click', function(){
+        paymentForm.style.display = 'block'
+    })
 }
 
-render()
+ function removeFromOrder(removeName){
+     allItemsAddedArr = allItemsAddedArr.filter(function(obj){
+         return (obj.name != removeName)
+    })
+    if (allItemsAddedArr.length){
+        renderOrder()
+    } else {
+        checkoutContainer.innerHTML = ``
+    }
+ }
+
+function calculateTotal(){
+    totalPrice = 0
+    allItemsAddedArr.forEach(function(obj){
+        totalPrice+= obj.price
+    })
+    return totalPrice
+}  
+
+renderMenu()
+
+paymentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    const orderFormData = new FormData(paymentForm)
+    const name = orderFormData.get('pay-name')
+    const cardNumber = orderFormData.get('pay-card')
+    const cvv = orderFormData.get('pay-cvv')
+    console.log(`The order for ${name} (payment info: ${cardNumber}, ${cvv}) consists of ${allItemsAddedArr.map(obj => obj.name).join(', ')}.`)
+    renderPaymentFinished(name)
+})
+
+function renderPaymentFinished(name){
+    paymentForm.style.display = 'none'
+    checkoutContainer.innerHTML = `<h1 id='thanks'>Thanks, ${name}! Your order is on its way!</h1>`
+}
